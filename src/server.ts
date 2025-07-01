04c227784e727f5dcf0de1ecb356a5a98982f399
MAIAN-HUNTER <maianlucas6@gmail.com>
2025-07-01 18:06:02 -0300

    adicionado swagger com cadastro de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/movies', async (_, res) => {
    const movies = await prisma.movies.findMany({
@@ -110,8 +113,6 @@ app.get("/movies/:genreName", async(req, res) => {
                     },
                  },
             });
-
-            //DESCOBRIR PQ ESSA PESTE N TA FILTRANDO OS FILMES POR GENERO!!!
 
                    res.status(200).send(moviesFilteredByGenreName);
    } catch (error) {
